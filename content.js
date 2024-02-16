2f298ce3324ef9ee45f28bd638ad2ce61b545f27
Erik Bryant <erikbryantology@gmail.com>
2024-02-16 08:07:14 -0500

    Move common code into separate function

--- content.js
+++ content.js
@@ -1,61 +1,61 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  switch(location.host) {
+    case 'www.wsj.com':
+      wsj();
+      break;
+    case 'www.nytimes.com':
+      nyTimes();
+      break;
+    default:
+      console.log('No sandpaper handler for:', location.host)
+  }
+});
 
-  let elements;
-
-  // Remove WSJ cloudfront-hosted ads
-  elements = document.getElementsByTagName("a");
+// Remove an attribute (or its parent) from the document
+function remove(elementType, attrName, attrValue, parents, note ) {
+  const elements = document.getElementsByTagName(elementType);
   for (let i = 0; i < elements.length; i++) {
-    let attr = elements[i].getAttribute("href");
-    if (attr !== null && attr.includes('.cloudfront.net')) {
-      let parent = elements[i].parentElement.parentElement.parentElement;
-      console.log('Removing cloudfront-hosted ad');
-      parent.remove();
+    let attr = elements[i].getAttribute(attrName);
+    if (attr !== null && attr.startsWith(attrValue)) {
+      // Walk up through the parents
+      let outer = elements[i];
+      for (let p = 0; p < parents; p++) {
+        outer = outer.parentElement
+      }
+      console.log(note);
+      outer.remove();
     }
   }
+}
 
-  // Remove WSJ tiktok videos
-  elements = document.getElementsByTagName("div");
-  for (let i = 0; i < elements.length; i++) {
-    let attr = elements[i].getAttribute("data-inset_type");
-    if (attr === "tiktok") {
-      console.log('Removing TikTok video');
-      elements[i].remove();
-    }
-  }
+// New York Times site smoothing
+function nyTimes() {
+  // Remove nav bar
+  remove('div', 'data-testid', 'masthead-nested-nav', 0, 'Removing nav bar');
 
-  // Remove WSJ nav bar
-  elements = document.getElementsByTagName("nav");
-  for (let i = 0; i < elements.length; i++) {
-    let attr = elements[i].getAttribute("aria-label");
-    if (attr === "Primary") {
-      console.log('Removing nav bar');
-      elements[i].remove();
-      elements[i].remove(); // I don't know it only works if we do this twice
-    }
-  }
+  // Remove slideshow animations
+  remove('div', 'class', 'slideshow-animate', 0, 'Removing slideshow');
 
-  // Remove WSJ home page ads and opinions
-  elements = document.getElementsByTagName("div");
-  for (let i = 0; i < elements.length; i++) {
-    let attr = elements[i].getAttribute("class");
-    if (attr !== null && attr.startsWith('WSJTheme--padding-bottom')) {
-      console.log('Removing top ad')
-      elements[i].remove();
-    }
-    if (attr !== null && attr.startsWith('WSJTheme--adWrapper')) {
-      console.log('Removing ad wrapper');
-      elements[i].remove();
-    }
-  }
+  // Remove auto-play videos
+  remove('section', 'aria-label', 'Gallery', 0, 'Removing video');
+}
+
+// Wall Street Journal site smoothing
+function wsj() {
+  // Remove cloudfront-hosted ads
+  remove('a', 'href', '.cloudfront.net', 0, 'Removing cloudfront-hosted ad');
+
+  // Remove tiktok videos
+  remove('div', 'data-inset_type', 'tiktok', 0, 'Removing TikTok video');
+
+  // Remove nav bar
+  remove('nav', 'aria-label', 'Primary', 0, 'Removing nav bar');
+  remove('nav', 'aria-label', 'Primary', 0, 'Removing nav bar');  // I don't know why we need to do this twice
+
+  // Remove home page ads and opinions
+  remove('div', 'class', 'WSJTheme--padding-bottom', 0, 'Removing top ad');
+  remove('div', 'class', 'WSJTheme--adWrapper', 0, 'Removing ad wrapper');
 
   // Remove embedded videos
-  elements = document.getElementsByTagName("div");
-  for (let i = 0; i < elements.length; i++) {
-    let attr = elements[i].getAttribute("data-type");
-    if (attr === "video") {
-      console.log('Removing embedded video');
-      elements[i].remove();
-    }
-  }
-
-});
+  remove('div', 'data-type', 'video', 0, 'Removing embedded video');
+}
